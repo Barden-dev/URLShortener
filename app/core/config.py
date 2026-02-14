66c784bcaebb2e7044ebdb857f4929d926cfc425
Barden <barden2410@gmail.com>
2026-02-14 23:36:21 +0300

    Created DataBase

---
+++ app/core/config.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+class AppBaseSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=BASE_DIR / ".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+
+    database_url : str
+
+settings = AppBaseSettings()
---
+++ app/models/url.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Integer, Boolean
+from app.core.database import Base
+
+class Url(Base):
+    __tablename__ = "urls"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    secret_key: Mapped[str] = mapped_column(String(16), unique=True, index=True)
+    target_url: Mapped[str] = mapped_column(String(2048))
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    clicks: Mapped[int] = mapped_column(Integer, default=0)

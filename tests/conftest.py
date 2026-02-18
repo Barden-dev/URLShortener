72d57595f8f4e33691e9f305e0ea31e0d744ef61
Barden <barden2410@gmail.com>
2026-02-18 14:08:30 +0300

    Fixed code with linters

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,18 @@
 import pytest
 import pytest_asyncio
-from httpx import AsyncClient, ASGITransport
+from httpx import ASGITransport, AsyncClient
 
 from app.main import app
+
 
 @pytest.fixture(scope="session")
 def async_backend():
     return "asyncio"
 
+
 @pytest_asyncio.fixture(scope="function")
 async def client():
-    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as c:
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as c:
         yield c
